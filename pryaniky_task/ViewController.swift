bbd3d970f3726289a1223acb33d698d241949acd
Teasty <kostoprav28@mail.ru>
2020-10-16 14:22:34 +0300

    Initial Commit

---
+++ pryaniky_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pryaniky_task
+//
+//  Created by Андрей Лихачев on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
